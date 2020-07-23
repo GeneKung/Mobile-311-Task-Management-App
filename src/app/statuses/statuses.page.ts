837ecb3d680fd4d184e241e08a2999c0df2be6f6
Johnnypham12373 <huypham12373@gmail.com>
2020-07-22 21:16:56 -0700

    Update statuses.page.ts

--- src/app/statuses/statuses.page.ts
+++ src/app/statuses/statuses.page.ts
@@ -11,7 +11,6 @@ export class StatusesPage implements OnInit {
   ios: boolean;
   tracks: {name: string, icon: string, isChecked: boolean}[] = [];
   constructor(    
-    private config: Config,
     public modalCtrl: ModalController,
     public navParams: NavParams,
     private activateRoute: ActivatedRoute
@@ -21,23 +20,5 @@ export class StatusesPage implements OnInit {
     this.statuses = this.activateRoute.snapshot.paramMap.get('id');
   }
 
-  selectAll(check: boolean) {
-    // set all to checked or unchecked
-    this.tracks.forEach(track => {
-      track.isChecked = check;
-    });
-  }
-
-  applyFilters() {
-    // Pass back a new array of track names to exclude
-    const excludedTrackNames = this.tracks.filter(c => !c.isChecked).map(c => c.name);
-    this.dismiss(excludedTrackNames);
-  }
-
-  dismiss(data?: any) {
-    // using the injected ModalController this page
-    // can "dismiss" itself and pass back data
-    this.modalCtrl.dismiss(data);
-  }
 }
 
