cb0d2ec29b8a9c3277999102fd2944107d7e8a33
GeneKung <52974083+GeneKung@users.noreply.github.com>
2020-08-10 13:55:09 -0700

    Search Page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonSelect } from '@ionic/angular';
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-
+  hideList = true;
+  countrySelectRef: any;
   constructor() { }
 
   ngOnInit() {
   }
-
+  displayCountry() {
+    this.countrySelectRef.open();
+ }
+ setCountry(){
+   
+ }
 }
