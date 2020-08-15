864cfb63e8b39bb4d2607f9d6cb7f0fc0f05c23e
GeneKung <52974083+GeneKung@users.noreply.github.com>
2020-08-14 20:06:16 -0700

    delete save function

--- src/app/createtask/createtask.page.ts
+++ src/app/createtask/createtask.page.ts
@@ -90,35 +90,6 @@ export class CreatetaskPage implements OnInit {
   }); 
   }
 
-  saveToList(){
-    this.listInfo['workGroup'] = this.getWorkGroup
-    this.listInfo['workType'] = this.getWorkType
-    this.listInfo['assetID'] = this.assetID
-    this.listInfo['department'] = this.getDepartment
-    this.listInfo['address'] = this.getAddress
-    this.listInfo['priority'] = this.getPriority
-    this.listInfo['description'] = this.getDescription
-    this.storeData(this.listInfo)
-    this.getWorkGroup = null
-    this.getWorkType = null
-    this.assetID = null
-    this.getDepartment = null
-    this.getAddress = null
-    this.getPriority = null
-    this.getDescription = null
-    this.router.navigate(['tasks']);
-    this.menuCtrl.enable(true);
-    this.listInfo = {};
-  }
- 
-  storeData(listInfo){
-    this.storage.set(`${this.listInfoID}`, JSON.stringify(listInfo));
-    console.log(this.listInfoID);
-    this.listInfoID += 1;
-    this.storage.set('listInfoID', this.listInfoID);
- 
-  }
-
   getByWorkGroup(getWorkGroup){
     console.log(getWorkGroup)
   }
